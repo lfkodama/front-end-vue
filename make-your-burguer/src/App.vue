20ccd37a7b091acc9c762e8bc0684615ecf38096
FernandoKodama <lfkodama@gmail.com>
2023-04-27 06:31:09 -0300

    Added new project Make-your-burger

---
+++ make-your-burguer/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar />
+  <router-view/>
+  <Footer />
+</template>
+
+<script>
+  import Navbar from './components/NavBar.vue';
+  import Footer from './components/Footer.vue';
+
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ make-your-burguer/src/views/OrderView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="order">
+    <h1>Esta é a página de pedidos</h1>
+  </div>
+</template>
