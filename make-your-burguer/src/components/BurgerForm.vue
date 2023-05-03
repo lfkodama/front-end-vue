f98ac4102f6ad4b2d7d7fc64bb7c956e0d2617a8
FernandoKodama <lfkodama@gmail.com>
2023-05-03 06:16:51 -0300

    Inserting data into DB Json Servb

--- make-your-burguer/src/components/BurgerForm.vue
+++ make-your-burguer/src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de Mensagem</p>
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="nome">Nome do cliente</label>
           <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite seu nome">
@@ -49,7 +49,7 @@ export default {
       pao: null,
       carne: null,
       opcionais: [],
-      status: "Solicitado",
+      //status: "Solicitado",
       msg: null
     }
   },
@@ -60,6 +60,36 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
+    },
+    async createBurger(e) {
+      e.preventDefault();
+
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado"
+      }
+
+      const dataJson = JSON.stringify(data);
+
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson
+      });
+
+      const res = await req.json();
+
+      // Colocar uma mensagem de sistema
+
+      // Limpar os campos do form
+      this.nome = "";
+      this.carne = "";
+      this.pao = "";
+      this.opcionais = "";
+
     }
   },
   mounted() {
