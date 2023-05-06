dc4d9ec5a5810a43704b167dd446bbd4aa2a7bc7
FernandoKodama <lfkodama@gmail.com>
2023-05-06 18:50:56 -0300

    Project Finished \o/

--- make-your-burguer/src/components/Dashboard.vue
+++ make-your-burguer/src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="burget-table">
+    <Message :msg="msg" v-show="msg"/>
     <div>
       <div id="burger-table-heading">
         <div class="order-id">#:</div>
@@ -11,22 +12,27 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{burger.id}}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salama</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
-            <option value="">Selecione</option>
+          <select name="status" class="status" @change="updateBurger($event, burger.id)">
+            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
+              {{ s.tipo }}
+            </option>
+
+            
           </select>
-          <button class="delete-btn">Cancelar</button>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
       </div>
     </div>
@@ -35,8 +41,78 @@
 </template>
 
 <script>
+  import Message from './Message.vue';
+
   export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+      return {
+        burgers: null,
+        burger_id: null,
+        status: [],
+        msg: null
+      }
+    },
+    methods: {
+      async getPedidos() {
+
+        const req = await fetch("http://localhost:3000/burgers");
+
+        const data = await req.json();
+
+        this.burgers = data;
+
+        // Resgatar os status dos pedidos
+        this.getStatus();
+      },
+      async getStatus() {
+        const req = await fetch("http://localhost:3000/status");
+
+        const data = await req.json();
+
+        this.status = data;
+      },
+      async deleteBurger(id) {
+        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+          method: "DELETE"
+        });
+
+        const res = await req.json();
+
+        // Colocar uma mensagem de sistema
+        this.msg = `O pedido foi deletado!`;
+        setTimeout(() => this.msg = "", 3000);
+
+        this.getPedidos();
+
+      },
+      async updateBurger(event, id) {
+
+        const option = event.target.value;
+
+        const dataJson = JSON.stringify({ status: option });
+
+        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+          method: "PATCH",
+          headers: {"Content-Type": "application/json"},
+          body: dataJson
+        });
+
+        const res = await req.json();
+
+        // Colocar uma mensagem de sistema
+        this.msg = `O pedido Nº ${res.id} foi alterado para ${res.status}!`;
+        setTimeout(() => this.msg = "", 3000);
+
+        console.log(res)
+      }
+    },
+    mounted () {
+      this.getPedidos();
+    },
+    components: {
+    Message
+  }
   }
 </script>
 
