c1a5df982c63a4ba3e8e6ad7ebaf2124f26c0048
FernandoKodama <lfkodama@gmail.com>
2023-05-27 10:20:04 -0300

    LoginView.vue

---
+++ vue-firebase-auth/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="E-mail" /><br />
+    <input type="password" placeholder="Password" /><br />
+    <button>Connect</button>
+    <p>Você ainda não possui uma conta? Crie uma aqui.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loginView",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
