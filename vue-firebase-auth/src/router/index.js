244f3459442098318b8759bfb01b429476015b4b
FernandoKodama <lfkodama@gmail.com>
2023-06-20 22:44:06 -0300

    First Vue Firebase Auth Commit

---
+++ vue-firebase-auth/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    { path: "/", component: () => import("../views/HomeView.vue") },
+    { path: "/register", component: () => import("../views/RegisterView.vue") },
+    { path: "/sign-in", component: () => import("../views/SignInView.vue") },
+    { path: "/feed", component: () => import("../views/FeedView.vue") },
+    {
+      path: "/about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    },
+  ],
+});
+
+export default router;
